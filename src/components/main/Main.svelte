ae70db89c2fc179862ee53802d995550beb24da2
gigon_choe <gigon_choe@tmax.co.kr>
2021-07-12 19:46:08 +0900

    [feat] 메인화면 헤더 및 탭 추가

---
+++ src/components/main/Main.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { HeaderTitle } from './components/headers';
+  import { TabList } from './components/tabs';
+</script>
+
+<main>
+  <HeaderTitle />
+  <TabList />
+</main>
+
+<style>
+  main {
+    background-color: #f2f2f2;
+  }
+</style>
