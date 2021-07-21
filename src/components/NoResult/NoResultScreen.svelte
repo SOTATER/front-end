ea46f86c49a54fccc7cc752ac35ad0cb19be5eda
minyoungyang <didalsdud@gmail.com>
2021-07-21 18:58:29 +0900

    [feat] 결과 없는 화면 component 구현

---
+++ src/components/NoResult/NoResultScreen.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	const titleText = 'PI.GG에 등록되지 않은 소환사입니다. 오타를 확인 후 다시 검색해주세요.';
+</script>
+
+<div class="no-result">
+	<h2 class="title-text">
+		{titleText}
+	</h2>
+</div>
+
+<style>
+	.no-result {
+		width: 970px;
+		margin: 0 auto;
+		padding-top: 50px;
+		text-align: left;
+		line-height: 2;
+	}
+	.title-text {
+		color: #242929;
+		font-size: 26px;
+		margin: 0;
+		text-align: center;
+	}
+</style>
