ca58d84f1f93cee23631a58f81b7aeea1c5164e6
gigon_choe <gigon_choe@tmax.co.kr>
2021-09-02 18:59:14 +0900

    [feat] pie chart 추가
    적당한 pie chart library없어서 직접 만듬

--- src/components/matchs/components/Expand/Team/MatchAnalysis/MatchAnalysis.svelte
+++ src/components/matchs/components/Expand/Team/MatchAnalysis/MatchAnalysis.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import PieChart from './PieChart.svelte';
 	import ProgressBar from './ProgressBar.svelte';
+
 	const img =
 		'https://opgg-static.akamaized.net/images/lol/champion/Camille.png?image=q_auto:best&v=1630555355';
 </script>
 
 <ProgressBar imgUrl={img} value={5000} percentage={65} isWin={true} />
+<PieChart percentage={(39 / 81) * 100} winnerValue={42} loserValue={39} />
---
+++ src/components/matchs/components/Expand/Team/MatchAnalysis/PieChart.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	export let percentage: number;
+	export let winnerValue: number;
+	export let loserValue: number;
+</script>
+
+<div class="Container">
+	<div
+		class="OuterCircle"
+		style={`background: conic-gradient(#c6443e 0% ${percentage}%, #1a78ae ${percentage}% 100%)`}
+	>
+		<div class="InnerCircle">
+			<span class="winner">{winnerValue}</span>
+
+			<div class="horizon" />
+			<span class="loser">{loserValue}</span>
+		</div>
+	</div>
+</div>
+
+<style>
+	.Container {
+	}
+	.OuterCircle {
+		width: 80px;
+		height: 80px;
+		border-radius: 50%;
+	}
+	.InnerCircle {
+		position: relative;
+		width: 70px;
+		height: 70px;
+		background: #ededed;
+		border-radius: 50%;
+		top: 5px;
+		left: 5px;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.winner {
+		text-align: center;
+		color: #1a78ae;
+		font-size: 12px;
+		font-weight: bold;
+	}
+	.loser {
+		text-align: center;
+		color: #c6443e;
+		font-size: 12px;
+		font-weight: bold;
+	}
+	.horizon {
+		height: 1px;
+		width: 60px;
+		margin: 3px 0;
+
+		background: #cdcdcd;
+	}
+</style>
