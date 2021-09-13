86985d6874e3adcbf0c3600febd19c23c203c82d
minyoungyang <didalsdud@gmail.com>
2021-09-13 21:30:26 +0900

    [feat] App이 mount 되었을 때, data dragon에서 version과 item을 가져오도록 구현

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,23 @@
 	import Router from 'svelte-spa-router';
 	import { Search } from './components/search';
 	import { Main } from './components/main';
-
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+	import { ApiConstants } from './apis/ApiConstants';
+	import type { Item } from './schema/api/items';
 	export const url = '';
+
+	onMount(async () => {
+		const version = await axios.get('https://ddragon.leagueoflegends.com/api/versions.json');
+		ApiConstants.version = (version.data as string[])[0];
+		const items = await axios.get(
+			`https://ddragon.leagueoflegends.com/cdn/${ApiConstants.version}/data/ko_KR/item.json`,
+		);
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+		ApiConstants.items = items.data.data as {
+			[id: number]: Item;
+		};
+	});
 </script>
 
 <Router
