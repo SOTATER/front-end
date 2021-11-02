8d5f2f20b110fd7df7383d5661b8967b8e3b6e4c
minyoungyang <didalsdud@gmail.com>
2021-11-02 14:52:11 +0900

    [feat] Item Image Component 구현

---
+++ src/components/image/ItemImage.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { ApiConstants } from '../../apis/ApiConstants';
+	import { popoverItem } from '../tooltip/Tooltip';
+	export let itemId: number;
+	// console.log(ApiConstants.spells[spellId].image.full);
+</script>
+
+<div class="rune-image" use:popoverItem={{ itemId }}>
+	<img
+		class="image"
+		src={'http://ddragon.leagueoflegends.com/cdn/' +
+			ApiConstants.version +
+			'/img/item/' +
+			ApiConstants.items[itemId].image.full}
+		alt={ApiConstants.items[itemId].name}
+	/>
+</div>
+
+<style>
+	.rune-image {
+		width: 22px;
+		height: 22px;
+		overflow: hidden;
+	}
+	.image {
+		width: 100%;
+		height: 100%;
+	}
+</style>
