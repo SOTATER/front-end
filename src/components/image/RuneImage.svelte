746fd66aa28adf60fd5c18fa0d6a2899991fd750
minyoungyang <didalsdud@gmail.com>
2021-11-02 14:53:03 +0900

    [feat] Rune Image 컴포넌트 구현

---
+++ src/components/image/RuneImage.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { ApiConstants } from '../../apis/ApiConstants';
+	import { popoverRune } from '../tooltip/Tooltip';
+	export let runeId: number;
+	console.log(ApiConstants.runes[runeId].icon);
+</script>
+
+<div class="rune-image" use:popoverRune={{ runeId }}>
+	<img
+		class="image"
+		src={'assets/images/' + ApiConstants.runes[runeId].icon}
+		alt={ApiConstants.runes[runeId].name}
+	/>
+</div>
+
+<style>
+	.rune-image {
+		width: 22px;
+		height: 22px;
+		overflow: hidden;
+		border-radius: 50%;
+	}
+	.image {
+		width: 100%;
+		height: 100%;
+	}
+</style>
