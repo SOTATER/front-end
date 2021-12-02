fa204ed6ed44155b89bb0f8ed876da448072a00c
minyoungyang <didalsdud@gmail.com>
2021-12-02 18:48:30 +0900

    [feat] 시즌별 Tier를 표현하는 ProfileHistoryTip 컴포넌트 구현

---
+++ src/components/profile/ProfileHistoryTip.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export const season = 'S2020';
+	export const tier = 'gold';
+</script>
+
+<li class="history-tip">
+	<b>{season}</b>
+	{' ' + tier}
+</li>
+
+<style>
+	.history-tip {
+		padding: 3px 4px 2px 4px;
+		margin-right: 3px;
+		background: #e0e3e3;
+		color: #657070;
+		font-size: 11px;
+		border: 1px solid #ced3d3;
+		border-radius: 3px;
+		list-style: none;
+		width: fit-content;
+	}
+</style>
