6e8693e7732e8c732bb46b1eab707b1a829e126c
minyoungyang <didalsdud@gmail.com>
2021-12-18 20:51:54 +0900

    [feat] 최근 7일 간 챔피언 별 승률 표 컴포넌트 구현

---
+++ src/components/left-side/LastSevenDays/LastSevenDaysItem.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { ApiConstants } from '../../../apis/ApiConstants';
+	import ChampionImageCircle from '../../image/champion/ChampionImageCircle.svelte';
+	import WinRatioGraph from './WinRatioGraph.svelte';
+	export let championName: string;
+	export let win: number;
+	export let lose: number;
+	const championKoreanName = ApiConstants.champions[championName].name;
+	const winRatio = (win * 100) / (win + lose);
+</script>
+
+<div class="ChampionWinRatioBox">
+	<div class="Face">
+		<ChampionImageCircle {championName} size={32} />
+	</div>
+	<div class="ChampionInfo">
+		<div class="ChampionName">{championKoreanName}</div>
+	</div>
+	<div class="WinRatio">{Math.round(winRatio)}%</div>
+	<div class="RatioGraph">
+		<WinRatioGraph {win} {lose} />
+	</div>
+</div>
+
+<style>
+	.ChampionWinRatioBox {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		border-bottom: 1px solid #cdd2d2;
+		background: #ededed;
+		color: #879292;
+		font-size: 12px;
+	}
+	.Face {
+		display: flex;
+		width: 44px;
+		padding: 5px 0 0 0;
+		flex-direction: row;
+		justify-content: flex-end;
+	}
+	.ChampionInfo {
+		display: flex;
+		width: 61px;
+		flex-direction: row;
+		align-items: center;
+	}
+	.ChampionName {
+		width: 100%;
+		margin-left: 10px;
+		color: #555e5e;
+		font-weight: bold;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		overflow: hidden;
+		text-decoration: none;
+	}
+	.WinRatio {
+		display: flex;
+		width: 40px;
+		align-items: center;
+		justify-content: center;
+	}
+	.RatioGraph {
+		display: flex;
+		height: 40px;
+		align-items: center;
+		padding-left: 6px;
+		padding-right: 12px;
+	}
+</style>
---
+++ src/components/left-side/LastSevenDays/WinRatioGraph.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	export let win: number;
+	export let lose: number;
+	const winRatio = (win * 100) / (win + lose);
+	const winWidth = Math.round(winRatio).toString() + '%';
+</script>
+
+<div class="Graph">
+	{#if win !== 0}
+		<div class="Fill Left" style="width: {winWidth}" />
+		<div class="Text Left">{win}승</div>
+	{/if}
+	{#if lose !== 0}
+		<div class="Fill Right" />
+		<div class="Text Right">{lose}패</div>
+	{/if}
+	{#if win !== 0 && lose !== 0}
+		<div class="Bar" style="left: {winWidth}" />
+	{/if}
+</div>
+
+<style>
+	.Graph {
+		width: 140px;
+		height: 20px;
+		position: relative;
+	}
+	.Fill {
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 100%;
+		border-radius: 3px;
+	}
+	.Fill.Left {
+		z-index: 2;
+		background: #3d95e5;
+		border: 1px solid #3480c6;
+	}
+	.Fill.Right {
+		width: 100%;
+		z-index: 1;
+		background: #ee5a52;
+		border: 1px solid #c6443e;
+	}
+	.Text {
+		position: absolute;
+		top: 0;
+		height: 100%;
+		font-size: 11px;
+		z-index: 3;
+		color: #f2f2f2;
+		line-height: 22px;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+	.Text.Left {
+		left: 6px;
+		text-align: left;
+	}
+	.Text.Right {
+		right: 5px;
+		text-align: right;
+	}
+	.Bar {
+		position: absolute;
+		top: 0;
+		height: 100%;
+		border-left: 1px solid #1a78ae;
+		border-right: 1px solid #c6443e;
+		z-index: 2;
+	}
+</style>
