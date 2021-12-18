3ae49a47553e59650d00caa0e5c75623c809c411
minyoungyang <didalsdud@gmail.com>
2021-12-18 20:47:09 +0900

    [feat] 챔피언 이미지 사이즈를 조절할 수 있도록 size를 props화

--- src/components/image/champion/ChampionImageCircle.svelte
+++ src/components/image/champion/ChampionImageCircle.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
 	import { ApiConstants } from '../../../apis/ApiConstants';
 	export let championName: string;
+	export let size = 46;
 	function getChampionImageUrl(name: string): string {
 		return `http://ddragon.leagueoflegends.com/cdn/${ApiConstants.version}/img/champion/${ApiConstants.champions[name].image.full}`;
 	}
 </script>
 
-<div class="champion-image">
+<div class="champion-image" style="width:{size}px;height:{size}px">
 	<img class="image" src={getChampionImageUrl(championName)} alt={championName} />
 </div>
 
 <style>
 	.champion-image {
-		width: 46px;
-		height: 46px;
 		overflow: hidden;
 		border-radius: 50%;
 	}
